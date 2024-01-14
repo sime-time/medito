81ed174456cdbbd85bfe8a1c5b50d750e5800bef
Dunn, Simeon <simedunn01@gmail.com>
2024-01-13 21:12:35 -0500

    goal-progress numbers/currency

--- medito-page/src/lib/Fundraiser.svelte
+++ medito-page/src/lib/Fundraiser.svelte
@@ -1,14 +1,19 @@
 <script>
   import Description from './Description.svelte'
   import GoalProgress from './GoalProgress.svelte'
+  let fundraiser_title = "Create Ad Campaign in the Tram"
 </script>
 
 
 <div class="container">
-  <h1>Hello Medito Hero</h1>
+  <h1 class="title">{fundraiser_title}</h1>
   <div class="fundraiser-info">
-    <Description />
-    <GoalProgress />
+    <div id="desc-container">
+      <Description />
+    </div>
+    <div id="goal-container">
+      <GoalProgress />
+    </div>
   </div>
 </div>
 
@@ -22,8 +27,22 @@
   }
 
   .fundraiser-info {
-    max-width: var(--max-width);
+    width: 100%;
     display: flex;
     flex-direction: row;
+    justify-content: space-between;
+    gap: 1em;
   }
+
+  .title {
+    padding-block: 0.5em;
+  }
+
+  #desc-container {
+    width: 100%;
+  }
+  #goal-container {
+    width: 100%;
+  }
+
 </style>
--- medito-page/src/lib/GoalProgress.svelte
+++ medito-page/src/lib/GoalProgress.svelte
@@ -1,15 +1,59 @@
 <script> 
   // take donation data from API 
+  const money_raised_api = 34000;
+  const money_goal_api = 55000;
+  const donations_api = 1420;
+
+  // format API data to $USD
+  const currency = Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    maximumFractionDigits: 0
+  });
+
+  let money_raised = currency.format(money_raised_api);
+  let money_goal = currency.format(money_goal_api);
+  let donations = currency.format(donations_api);
+
+
 </script>
 
 
-<div class="goal-progress">
-  <h3 class="">Goal Progress Bar</h3>  
+<div class="goal-progress-container">
+  <div class="goal-content">
+    
+
+    <div class="goal-numbers">
+      <h2 class="small-title">{money_raised}
+       <span class="subtext"> raised of {money_goal} goal!</span>
+      </h2>
+    </div>
+    <div class="goal-bar">
+    </div>
+
+
+  </div>
 </div>
 
 <style>
-  .goal-progress {
+  .goal-progress-container {
     padding: 1em;
-
+    background: var(--tertiary);
+    border-radius: var(--border-radius);
   }
-</style>
+  
+  .goal-content {
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+  }
+  .goal-numbers {
+    display: flex;
+    flex-direction: row;
+  }
+  .subtext {
+    color: var(--primary);
+    font-size: 16px;
+  }
+
+</style> 
